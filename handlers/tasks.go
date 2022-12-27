ce87c67376878d4b987b9781ad818bc08bd2b516
Krishna <iamkrishna.krishna@gmail.com>
2022-12-27 17:59:03 +0530

    Get task bug

--- handlers/tasks.go
+++ handlers/tasks.go
@@ -108,7 +108,7 @@ func insertTask(task *model.Task) (string, error) {
 
 func getTasks(getTaskInput *model.GetTaskInput, w *http.ResponseWriter) []model.Task {
 	var tasks []model.Task
-	err := pgxscan.Select(context.Background(), global.DbConn, &tasks, `SELECT * from task where user_uuid = $1`, getTaskInput.UserUuid)
+	err := pgxscan.Select(context.Background(), global.DbConn, &tasks, `SELECT * from task where user_uuid = '$1'`, getTaskInput.UserUuid)
 	if err != nil {
 		slog.Error("Get task: Error in getting tasks from db", err)
 		http.Error(*w, "Get task: Error in getting tasks from db: "+err.Error(), 500)
